3658febcca227aed01350bb55d4f0cd5e0a74b38
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2020-12-09 00:07:06 +0200

    Refactored Downloader class, now has methods instead of static functions.

--- download.py
+++ download.py
@@ -5,56 +5,63 @@ import urllib.request
 
 
 class Downloader:
+    """Class for downloading files in a specific directory.
 
-    @staticmethod
-    def get(url, dest_dir="data/"):
+    Attributes:
+        dest_dir: The destination directory where every file is stored.
+
+    """
+
+    def __init__(self, dest_dir="data/"):
+        self.dest_dir = pathlib.Path(dest_dir)
+
+    def get(self, url):
         """Gets the file corresponding to the given URL.
 
         Args:
             url (str): URL of the file.
-            dest_dir (str): Destination directory of the file. Defaults to
-                "data/".
 
         """
-        dest_dir = pathlib.Path(dest_dir)
-        filename = pathlib.Path(dest_dir, url.split("/")[-1])
-        if not dest_dir.exists():
-            dest_dir.mkdir()
+
+        if not self.dest_dir.exists():
+            self.dest_dir.mkdir()
+
+        filename = self.dest_dir.joinpath(url.split("/")[-1])
         urllib.request.urlretrieve(url, filename)
 
-    @staticmethod
-    def _unzip(filename):
+    def _unzip(self, filename):
         """Replaces the given file with an unzipped one.
 
         Args:
-            filename (str): The file path.
+            filename (pathlib.Path): The file path.
         """
-        filename = pathlib.Path(filename)
+
         new_filename = filename.with_suffix("")
+
         with gzip.open(filename, 'rb') as f_in:
             with open(new_filename, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
+
         filename.unlink()
 
-    @staticmethod
-    def get_unzip(url, dest_dir="data/"):
+    def get_unzip(self, url):
         """Gets and unzips the file corresponding to the given URL.
 
-        Keeps only the unzipped file.
+        Keeps only the unzipped file. 
 
         Args:
             url (str): URL of the file.
-            dest_dir (str): Destination directory of the file. Defaults to
-                "data/".
 
         """
-        filename = pathlib.Path(dest_dir, url.split("/")[-1])
-        Downloader.get(url, dest_dir)
-        Downloader._unzip(filename)
+
+        self.get(url)
+        filename = self.dest_dir.joinpath(url.split("/")[-1])
+        self._unzip(filename)
 
 
 if __name__ == "__main__":
 
     # example use
     url = 'http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/AMAZON_FASHION_5.json.gz'
-    Downloader.get_unzip(url)
+    downloader = Downloader()
+    downloader.get_unzip(url)
