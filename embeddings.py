1b59521827dcdc58ab27b76510e923ab36f1ce7e
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-01-10 22:36:28 +0200

    Refactored main into sklearn compatible transformer class.

--- embeddings.py
+++ embeddings.py
@@ -3,6 +3,7 @@ from sklearn import metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 from extract import Extractor
@@ -22,6 +23,36 @@ def load_data():
     return X, y
 
 
+class Doc2VecVectorizer:
+    def __init__(self):
+        self.preprocess = Preprocess()
+        
+    def fit(self, X, y):
+        vocabulary = []
+        for doc in X:
+            lemmatized_doc = self.preprocess.lemmatize(doc)
+            vocabulary.append(lemmatized_doc)
+            
+        documents = []
+        for i, doc in enumerate(vocabulary):
+            tagged_doc = TaggedDocument(doc, [i])
+            documents.append(tagged_doc)
+
+        model = Doc2Vec(documents, vector_size=20, window=2, workers=4)
+        self.model = model
+
+        return self
+
+    def transform(self, X):
+        embeddings = []
+        for doc in X:
+            lemmatized_doc = self.preprocess.lemmatize(doc)
+            emb = self.model.infer_vector(lemmatized_doc)
+            embeddings.append(emb)
+            
+        return embeddings
+
+
 if __name__ == '__main__':
 
     # Read dataset
@@ -29,37 +60,9 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=14)
 
-    vocabulary = []
-    preprocess = Preprocess()
-
-    """
-    This is just once
-    """
-    for doc in X_train:
-        lemmatized_doc = preprocess.lemmatize(doc)
-        vocabulary.append(lemmatized_doc)
-
-    # Create TaggedDocuments
-    documents = []
-    for i, doc in enumerate(vocabulary):
-        tagged_doc = TaggedDocument(doc, [i])
-        documents.append(tagged_doc)
-    model = Doc2Vec(documents, vector_size=20, window=2, workers=4)
-    model.save("my-doc2vec-model")
-
-    model = Doc2Vec.load("my-doc2vec-model")
-
-    train_embeddings = [embedding for embedding in model.docvecs.vectors_docs]
-
-    test_embeddings = []
-    for doc in X_test:
-        lemmatized_doc = preprocess.lemmatize(doc)
-        emb = model.infer_vector(lemmatized_doc)
-        test_embeddings.append(emb)
-
-    clf = LogisticRegression()
-    clf.fit(train_embeddings, y_train)
-    y_pred = clf.predict(test_embeddings)
+    clf = make_pipeline(Doc2VecVectorizer(), LogisticRegression())
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
 
     print(f"Accuracy: {metrics.accuracy_score(y_test, y_pred)}")
     print(f"F1-score: {metrics.f1_score(y_test, y_pred, average='macro')}")
