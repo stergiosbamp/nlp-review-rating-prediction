9dc89c2c891f54a1acc1b98c3a71485606474591
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-01-12 11:52:00 +0200

    Implemented Resamper and Oversampler classes to deal with class imbalance.

---
+++ resample.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from imblearn.over_sampling import RandomOverSampler
+
+
+class Resampler:
+
+    def __init__(self, resampler):
+        self.resampler = resampler
+
+    def resample(self, X, y):
+        X_res, y_res = self.resampler.fit_resample(
+            np.reshape(X.to_numpy(), (-1, 1)), y)
+
+        docs = []
+        for i in range(X_res.shape[0]):
+            docs.append(X_res[i, 0])
+
+        return docs, y_res
+
+
+class Oversampler(Resampler):
+
+    def __init__(self):
+        super(Oversampler, self).__init__(RandomOverSampler())
