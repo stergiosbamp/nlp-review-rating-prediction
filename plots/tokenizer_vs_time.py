42e3aa80579af324a068c7a11a13dcac9edd6f29
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-01-16 20:20:33 +0200

    Updated plot for tokenizer vs time.

--- plots/tokenizer_vs_time.py
+++ plots/tokenizer_vs_time.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -10,19 +11,30 @@ def plot_tokenizers_vs_time(vectorizers_time, classifier):
     plt.yscale('log')
     plt.show()
 
+def plot_tokenizers_vs_time_all_in_one():
+    tokenizers = ("TfIdfVectorizer", "TfIdfVectorizer (Lemmatization)", "TfIdfVectorizer (Stemming)")
 
+    logistic_regression_times = (1.7131, 35.9588, 226.3316)
+    linear_svm_times = (2.4967, 32.4222, 225.6811)
+    naive_bayes_times = (0.9424, 30.7023, 235.69)
+
+    ind = np.arange(len(tokenizers))  # the x locations for the groups
+    width = 0.4  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(ind - width * 2 / 6, logistic_regression_times, width / 3, label="Logistic Regression")
+    rects2 = ax.bar(ind, linear_svm_times, width / 3, label="Linear SVM")
+    rects3 = ax.bar(ind + width * 2 / 6, naive_bayes_times, width / 3, label="Naive Bayes")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    plt.ylabel("Train and test time (secs)")
+    plt.yscale('log')
+    plt.title("Train and test time per Vectorizer")
+    ax.set_xticks(ind)
+    ax.set_xticklabels(tokenizers)
+    ax.legend()
+
+    plt.show()
 
 if __name__ == "__main__":
-    LABELS = ['TfIdfVectorizer', 'TfIdfVectorizer (Lemmatization)', 'TfIdfVectorizer (Stemming)']
-
-    # Naive Bayes classifier
-    naive_bayes_vectorizers_time = [0.9424, 30.7023, 235.69]
-    plot_tokenizers_vs_time(naive_bayes_vectorizers_time, "Naive Bayes")
-
-    # Logistic regression classifier
-    logistic_vectorizers_time = [1.7131, 35.9588, 226.3316]
-    plot_tokenizers_vs_time(logistic_vectorizers_time, "Logistic Regression")
-
-    # Linear SVM
-    svm_vectorizers_time = [2.4967, 32.4222, 225.6811]
-    plot_tokenizers_vs_time(svm_vectorizers_time, "Linear SVM")
+    plot_tokenizers_vs_time_all_in_one()
