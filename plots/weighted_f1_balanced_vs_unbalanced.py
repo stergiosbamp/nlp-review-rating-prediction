30305e47b0aaa3420f8a541d0df88a8771f901e6
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-01-16 19:49:14 +0200

    Added plot for unbalanced vs balanced f1-score

---
+++ plots/weighted_f1_balanced_vs_unbalanced.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+models = ("Logistic Regression", "Linear SVM", "Naive Bayes")
+
+unbalanced_f1s = (0.5337, 0.5129, 0.4125)
+balanced_f1s = (0.5442, 0.5264, 0.5028)
+
+men_means = (20, 35, 30, 35, 27)
+women_means = (25, 32, 34, 20, 25)
+
+ind = np.arange(len(models))  # the x locations for the groups
+width = 0.4  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind - width / 4, unbalanced_f1s, width / 2, label="Unbalanced data")
+rects2 = ax.bar(ind + width / 4, balanced_f1s, width / 2, label="Balanced data")
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('F1-score')
+ax.set_title('F1-score for unbalanced and balanced dataset per classifier')
+ax.set_xticks(ind)
+ax.set_xticklabels(models)
+ax.legend(loc="upper right")
+
+plt.show()
