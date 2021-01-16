357fbb5f29666faac90394bd0563c120e5b84ceb
stergiosbamp <bampakis.stergios@gmail.com>
2021-01-16 20:03:27 +0200

    Plot for class distributions

---
+++ plots/class_distribution.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from extract import Extractor
+
+
+if __name__ == "__main__":
+    filename = "../data/Software_5.json"
+    data_fields = ["reviewText"]
+    target_field = "overall"
+    df = Extractor.extract_examples(filename,
+                                      data_fields,
+                                      target_field,
+                                      drop_duplicates=True)
+    
+    df_target = df['overall']
+
+    counts = df_target.value_counts()
+    counts.plot.pie()
+    
+    plt.show()
